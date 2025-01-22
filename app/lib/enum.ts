c9c9504645de340d38f09dbea97689ec8b0f1cb6
zjc <1569294420@qq.com>
2025-01-21 23:08:58 -0800

    fix:modify AI the default of ERNIE_BOT_TURBO variable

--- app/lib/enum.ts
+++ app/lib/enum.ts
@@ -289,6 +289,6 @@ export enum E_Public {
 export enum E_FOUNDATION_MODEL {
   GPT_35_TURBO = 'gpt-3.5-turbo', // openai
   Local_GPT = 'loacl-compatible-gpt-3.5', //本地兼容opanai-api接口的 大语言模型，如chatGLM6b,通义千问 等。
-  ERNIE_BOT_TURBO = 'ERNIE-Bot-turbo', // 文心一言
+  ERNIE_BOT_TURBO = 'ERNIE-4.0-8K', // 文心一言
   MOONSHOT_V1_8K = 'moonshot-v1-8k' // kimi
 }
