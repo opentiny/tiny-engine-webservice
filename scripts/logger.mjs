60b456d771fe73de046ecfeaa225bff601a949a0
lu17301156525 <lu17301156525@163.com>
2025-07-23 02:14:40 -0700

    feat: add sql script

---
+++ scripts/logger.mjs
@@ -0,0 +1,42 @@
+import colors from 'picocolors'
+
+class Logger {
+  constructor(command) {
+    this.command = command
+  }
+
+  output(type, msg) {
+    const format = () => {
+      const colorMap = {
+        info: 'cyan',
+        warn: 'yellow',
+        error: 'red',
+        success: 'green'
+      }
+      const time = new Date().toLocaleTimeString()
+      const colorMsg = colors[colorMap[type]](type)
+
+      return `[${this.command}] [${colors.dim(time)}] ${colorMsg} ${msg}`
+    }
+
+    return console.log(format())
+  }
+
+  info(msg) {
+    this.output('info', msg)
+  }
+
+  warn(msg) {
+    this.output('warn', msg)
+  }
+
+  error(msg) {
+    this.output('error', msg)
+  }
+
+  success(msg) {
+    this.output('success', msg)
+  }
+}
+
+export default Logger
