43a846dcfd9a86e9ab278daee9faf89e24447079
lichunn <46984087+lichunn@users.noreply.github.com>
2025-08-29 18:13:34 +0800

    feat：Change the AI interface to openai format (#31)

--- app/controller/app-center/aiChat.ts
+++ app/controller/app-center/aiChat.ts
@@ -10,27 +10,76 @@
  *
  */
 import { Controller } from 'egg';
-import { E_FOUNDATION_MODEL } from '../../lib/enum';
 
 export default class AiChatController extends Controller {
   public async aiChat() {
     const { ctx } = this;
-    const { foundationModel, messages } = ctx.request.body;
-    this.ctx.logger.info('ai接口请求参参数 model选型:', foundationModel);
+    const options = ctx.request.body;
+    this.ctx.logger.info('ai接口请求参数 model选型:', options);
+
+    const messages = options.messages;
     if (!messages || !Array.isArray(messages)) {
       return this.ctx.helper.getResponseData('Not passing the correct message parameter');
     }
-    const model = foundationModel?.model ?? E_FOUNDATION_MODEL.GPT_35_TURBO;
-    const token = foundationModel.token;
-    ctx.body = await ctx.service.appCenter.aiChat.getAnswerFromAi(messages, { model, token });
+    const apiKey = ctx.request.header?.authorization?.replace('Bearer', '');
+    const baseUrl = options?.baseUrl;
+    const model = options?.model;
+    const stream = options?.stream || false;
+    const tools = options?.tools || [];
+
+    if (stream) {
+      ctx.status = 200;
+      ctx.set({
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        Connection: 'keep-alive'
+      });
+      try {
+        const result = await ctx.service.appCenter.aiChat.getAnswerFromAi(messages, {
+          apiKey,
+          baseUrl,
+          model,
+          stream,
+          tools
+        });
+
+        for await (const chunk of result) {
+          ctx.res.write(`data: ${JSON.stringify(chunk)}\n\n`); // SSE 格式
+        }
+
+        // 添加结束标记
+        ctx.res.write('data: [DONE]');
+      } catch (e: any) {
+        this.ctx.logger.error(`调用AI大模型接口失败: ${(e as Error).message}`);
+      } finally {
+        console.log('end');
+        ctx.res.end(); // 关闭连接
+      }
+
+      return;
+    }
+
+    // 非流式模式
+    ctx.body = await ctx.service.appCenter.aiChat.getAnswerFromAi(messages, {
+      apiKey,
+      baseUrl,
+      model,
+      stream,
+      tools
+    });
   }
 
+  public async search() {
+    const { ctx } = this;
+    const { content } = ctx.request.body;
+
+    ctx.body = await ctx.service.appCenter.aiChat.search(content);
+  }
 
   public async uploadFile() {
     const { ctx } = this;
-    const fileStream = await ctx.getFileStream();
-    const foundationModelObject = JSON.parse(fileStream.fields.foundationModel);
-    const { model, token } = foundationModelObject.foundationModel;
-    ctx.body = await ctx.service.appCenter.aiChat.getFileContentFromAi(fileStream, { model, token });
+    const stream = await ctx.getFileStream();
+
+    ctx.body = await ctx.service.appCenter.aiChat.uploadFile(stream);
   }
 }
